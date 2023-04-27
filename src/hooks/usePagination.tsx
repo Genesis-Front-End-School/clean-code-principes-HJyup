904707e886c1c20035af75faf2925db698c625b6
HJK <supermaster98z@gmail.com>
2023-04-27 16:09:30 +0100

    fix: added usePagination hook

---
+++ src/hooks/usePagination.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+const usePagination = () => {
+  const [pagesCount, setPagesCount] = useState(0);
+
+  const handleChangeElement = (
+    event: React.ChangeEvent<unknown>,
+    value: number,
+  ) => {
+    setPagesCount(value - 1);
+  };
+
+  return [pagesCount, handleChangeElement] as const;
+};
+
+export default usePagination;
