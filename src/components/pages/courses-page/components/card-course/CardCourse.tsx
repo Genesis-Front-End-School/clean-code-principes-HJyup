668f0c28e881131ba6a97af3f917bd08b88f9723
HJK <supermaster98z@gmail.com>
2023-05-01 17:55:13 +0100

    fix: changed parameters structure and added type for CardCourse

--- src/components/pages/courses-page/components/card-course/CardCourse.tsx
+++ src/components/pages/courses-page/components/card-course/CardCourse.tsx
@@ -2,80 +2,67 @@ import React, { useState } from 'react';
 import { Card, Chip, Rating } from '@mui/material';
 import Link from 'next/link';
 
+import { GetCourseDTO } from '@/api/types/getCoursesDTO';
 import Video from '@/components/common/video';
+import { getCourseCardData } from '@/components/pages/courses-page/components/card-course/utils/getCourseCardData';
 import { fixImagePath } from '@/utility';
 
 import styles from './CardCourse.module.scss';
 
 export interface CardCourseProps {
-  image: string;
-  courseId: string;
-  title: string;
-  tags: string;
-  rating: number;
-  description: string;
-  skills: string[];
-  previewVideo: string;
-  lessonsCount?: number;
+  course: GetCourseDTO;
 }
 
-const CardCourse: React.FC<CardCourseProps> = ({
-  image,
-  courseId,
-  title,
-  tags,
-  rating,
-  description,
-  skills,
-  previewVideo,
-  lessonsCount,
-}) => {
+const CardCourse: React.FC<CardCourseProps> = ({ course }) => {
   const [isVideo, setIsVideo] = useState(false);
+  const courseCard = getCourseCardData(course);
 
-  const skillSelection = skills?.map((skill, index) => (
+  const skillSelection = courseCard.skills.map((skill, index) => (
     <p key={index}>{skill}</p>
   ));
 
   const handleMouseEnter = () => {
     if (isVideo) {
-      return <Video source={previewVideo} hasControls={false} />;
+      return <Video source={courseCard.previewVideo} hasControls={false} />;
     }
   };
 
   return (
-    <Link href={`/course/${courseId}`} className={styles['link']}>
+    <Link href={`/course/${courseCard.id}`} className={styles['link']}>
       <Card
         className={styles['card']}
         onMouseEnter={() => setIsVideo(true)}
         onMouseLeave={() => setIsVideo(false)}
       >
         <img
-          src={fixImagePath(image)}
+          src={fixImagePath(courseCard.previewImageLink)}
           className={styles['image']}
           alt="Course preview image"
         />
         <div className={styles['video']}>{handleMouseEnter()}</div>
         <div className={styles['information']}>
-          <div className={styles['title']}>{title}</div>
+          <div className={styles['title']}>{courseCard.title}</div>
           <div className={styles['tags']}>
             <Chip
-              label={tags}
+              label={courseCard.tags}
               color="primary"
               variant="outlined"
               className={styles['chip']}
             />
             <Rating
               name="Course Rating"
-              value={rating}
+              value={courseCard.rating}
               readOnly
               className={styles['rating']}
               precision={0.5}
             />
           </div>
-          <div className={styles['description']}>{description}</div>
+          <div className={styles['description']}>{courseCard.description}</div>
           <div className={styles['skills']}>{skillSelection}</div>
         </div>
-        <div className={styles['lessons-count']}>Lessons: {lessonsCount}</div>
+        <div className={styles['lessons-count']}>
+          Lessons: {courseCard.lessonsCount}
+        </div>
       </Card>
     </Link>
   );
