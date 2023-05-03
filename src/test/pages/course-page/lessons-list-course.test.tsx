f89ee464ae9b15ac74e63ec7d4c256c2cf6751ec
HJK <supermaster98z@gmail.com>
2023-05-03 10:21:57 +0100

    feature: added more tests

--- src/test/pages/course-page/lessons-list-course.test.tsx
+++ src/test/pages/course-page/lessons-list-course.test.tsx
@@ -7,12 +7,33 @@ import { testCourseData } from '@/test/const/testCourseData';
 import '@testing-library/jest-dom';
 
 describe('Describe lessons list course', () => {
-  it('It should check that data are correspond to values', () => {
+  beforeEach(() => {
     render(<LessonsListCourse course={testCourseData} setLesson={jest.fn()} />);
+  });
+
+  it('It should check that data are correspond to values', () => {
     const lessons = screen.getAllByRole('lesson');
 
     lessons.forEach((lesson, index) => {
       expect(lesson).toHaveTextContent(testCourseData.lessons[index].title);
+    });
+  });
+
+  it('It should check that data length is correct', () => {
+    const lessons = screen.getAllByRole('lesson');
+
+    expect(lessons.length).toBe(testCourseData.lessonsCount);
+  });
+
+  it('It should check that video has correct classNames', () => {
+    const lessons = screen.getAllByRole('lesson');
+
+    lessons.forEach((lesson, index) => {
+      expect(lesson).toHaveClass(
+        testCourseData.lessons[index].status === 'unlocked'
+          ? 'list-content'
+          : 'list-content-locked',
+      );
     });
   });
 });
